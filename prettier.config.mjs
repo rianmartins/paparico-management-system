6129136f8ad5f560d4fe6030bd56d0d192cff610
Rian Martins <rianlopes@hotmail.com>
2026-03-21 18:38:07 +0000

    core: Initial project setup

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const prettierConfig = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 120,
+  endOfLine: 'lf'
+};
+
+export default prettierConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,30 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        <Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={100} height={20} priority />
         <div className={styles.intro}>
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+            Looking for a starting point or more instructions? Head over to{' '}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               Templates
-            </a>{" "}
-            or the{" "}
+            </a>{' '}
+            or the{' '}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
               Learning
-            </a>{" "}
+            </a>{' '}
             center.
           </p>
         </div>
@@ -42,13 +35,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
+            <Image className={styles.logo} src="/vercel.svg" alt="Vercel logomark" width={16} height={16} />
             Deploy Now
           </a>
           <a
