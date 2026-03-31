245804174c85650082524b1773895526042f846d
Rian Martins <rianlopes@hotmail.com>
2026-03-31 21:54:38 +0100

    Fix configuration issue when running cypress component tests on github pipeline

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,10 @@
-import path from 'node:path';
-
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
-      viteConfig: {
-        resolve: {
-          dedupe: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
-          alias: {
-            '@': path.resolve(__dirname, 'src')
-          }
-        }
-      }
+      framework: 'next',
+      bundler: 'webpack'
     },
     specPattern: 'src/**/*.cy.{ts,tsx}',
     supportFile: 'cypress/support/component.ts'
