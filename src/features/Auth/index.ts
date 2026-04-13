4876ee22198cfbd7fe329a22e72687f656d8a9b0
Rian Martins <rianlopes@hotmail.com>
2026-04-13 20:11:06 +0100

    Add missing change

--- src/features/Auth/index.ts
+++ src/features/Auth/index.ts
@@ -2,6 +2,7 @@ export { default as AuthGuard } from './AuthGuard';
 export { default as LoginForm } from './LoginForm';
 export { default as LogoutButton } from './LogoutButton';
 export { default as RequiredPasswordUpdateModal } from './RequiredPasswordUpdateModal';
+export { currentUserQueryKey, currentUserQueryOptions, useCurrentUserQuery } from './query';
 export {
   AUTH_SESSION_STORAGE_KEY,
   clearStoredSession,
